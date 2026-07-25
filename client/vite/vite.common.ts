6c136ae13fd6a520a7b84559613883d5b48d751f
XNOR <XNORIGC@Gmail.com>
2026-07-25 01:26:47 +0000

    meow

--- client/vite/vite.common.ts
+++ client/vite/vite.common.ts
@@ -9,9 +9,9 @@ import { imageSpritesheet } from "./plugins/image-spritesheet-plugin";
 import { audioSpritesheet } from "./plugins/audio-spritesheet-plugin";
 
 const commonConfig: UserConfig = {
-    // base: "/custom/",
+    base: "/custom/",
     server: {
-        port: 3000,
+        // port: 3000,
         host: "0.0.0.0",
         allowedHosts: ["as.suroi.io"],
     },
